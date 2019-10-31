a04552a7f173e0940f663077d28043b7b15a065c
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2019-10-31 20:50:58 +0100

    Add arg for dtw file selection

--- dtw_distance.py
+++ dtw_distance.py
@@ -1,9 +1,19 @@
 import numpy as np
 from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
+import argparse
 
-matrix_1 = np.array(np.loadtxt(fname="matrix_1.txt"))
-matrix_2 = np.array(np.loadtxt(fname="matrix_2.txt"))
+parser = argparse.ArgumentParser()
+parser.add_argument('--first_id', type=int, default=1)
+parser.add_argument('--second_id', type=int, default=2)
+
+args = parser.parse_args()
+
+first_id = args.first_id
+second_id = args.second_id
+
+matrix_1 = np.array(np.loadtxt(fname="matrix_%d.txt" %first_id))
+matrix_2 = np.array(np.loadtxt(fname="matrix_%d.txt" %second_id))
 
 distance, path = fastdtw(matrix_1, matrix_2, dist=euclidean)
 
